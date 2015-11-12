da6da4a6b1b32d20c351f0db233979dfd38781e5
Dirk1 <Dirk2@Dirk2>
2015-11-12 20:36:10 +0100

    added new class and a constructor

---
+++ SalesTool/src/ca/demo/salestool/kunde.java
@@ -0,0 +1,11 @@
+package ca.demo.salestool;
+public class kunde {
+	String nachname;
+	String vorname;
+	
+	//Konstruktor
+	public Kunde (String pnachname, String pvorname) {
+		pnachname= nachname;
+		pvorname= pvorname;
+ }
+}
